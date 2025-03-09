878633949b34b9eccdcfcd01096cab8ae38107a1
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-09 07:15:36 +0530

    Merge Sort

--- src/Merge_Sort/Merge_sort.java
+++ src/Merge_Sort/Merge_sort.java
@@ -1,5 +1,7 @@
 package Merge_Sort;
 
+import Practice.Array;
+
 import java.util.ArrayList;
 
 public class Merge_sort {
@@ -18,9 +20,39 @@ public class Merge_sort {
         }
     }
     static void mergeSort(int[] arr, int low, int high){
-
+        if(low >= high) return;
+        int mid = (low + high )/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
     }
     static void merge(int[] arr,int low, int mid, int high){
-        
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while(right <= high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i<=high;i++){
+            arr[i] = temp.get(i - low);
+        }
     }
 }
