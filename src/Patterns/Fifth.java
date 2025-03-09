7e5ed9e4b4918aa38a6623a99b093837e22b394b
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-09 07:34:26 +0530

    Patterns

---
+++ src/Patterns/Fifth.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class Fifth {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i > 0 ; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
