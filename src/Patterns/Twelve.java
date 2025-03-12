e8e44db2eea898b12fcd33e28b983ef83e558b6c
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-12 17:01:59 +0530

    add new pattern

---
+++ src/Patterns/Twelve.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class Twelve {
+    public static void main(String[] args) {
+        int n = 4;
+        int x = 1;
+        for (int i = 0; i < n; i++) {
+            int temp = x;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(temp);
+                temp++;
+            }
+
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                temp--;
+                System.out.print(temp);
+            }
+//            temp = x;
+            System.out.println();
+        }
+    }
+}
