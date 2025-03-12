e4757aa46680d44acf73a3953b0179f660780d9c
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-12 15:49:25 +0530

    new pattern

---
+++ src/Patterns/Nineteen.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+public class Nineteen {
+    public static void main(String[] args) {
+        int n = 5;
+        part1(n);
+        part2(n);
+    }
+    static void part1(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void part2(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
