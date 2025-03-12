42e41ca7cd153f60b7d212820e92bb8bbf0d5ebd
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-12 12:22:08 +0530

    more patterns

---
+++ src/Patterns/Eleven.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+public class Eleven {
+    public static void main(String[] args) {
+        int n = 5;
+        eleven(n);
+    }
+
+     static void eleven(int n) {
+        int num = 1;
+         for (int i = 1; i <= n; i++) {
+             if(i%2 == 0) num = 0;
+             else num = 1;
+             for (int j = 1; j <= i; j++) {
+                 System.out.print(num);
+                 num = 1 - num;
+             }
+             System.out.println();
+         }
+    }
+}
---
+++ src/Patterns/Thirteen.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Thirteen {
+    public static void main(String[] args) {
+        int n = 5;
+        thirteen(n);
+    }
+
+     static void thirteen(int n) {
+        int num = 1;
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++){
+                System.out.print(" "+num);
+                num++;
+            }
+             System.out.println();
+         }
+    }
+}
