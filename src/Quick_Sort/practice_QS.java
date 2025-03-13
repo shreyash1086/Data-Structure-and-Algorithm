99a1e5a6e247bb0decc30676b7fd5f67fb5efd00
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-03-13 10:02:45 +0530

    Learning Quick Sort

---
+++ src/Quick_Sort/practice_QS.java
@@ -0,0 +1,52 @@
+package Quick_Sort;
+
+public class practice_QS {
+    public static void main(String[] args) {
+        int[] arr = {3,7,1,9,2,5,3,8,7,6};
+        System.out.println("before");
+        for(int x: arr){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+        QuickSort(arr,0,arr.length-1);
+        System.out.println("after");
+        for(int x: arr){
+            System.out.print(x+" ");
+        }
+    }
+    static void QuickSort(int[] arr,int low, int high){
+        if(low < high){
+            int partition_index = partition(arr,low,high);
+            QuickSort(arr,low,partition_index-1);
+            QuickSort(arr,partition_index+1,high);
+        }
+    }
+    static int partition(int[] arr,int low, int high){
+        int pivot = low;
+        int i = low;
+        int j = high;
+
+        while (i < j){
+
+            while(arr[i] <= arr[pivot] && i <= high-1){
+                i++;
+            }
+
+            while(arr[j] > arr[pivot] && j >= low+1){
+                j--;
+            }
+
+            if(i<j){
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,pivot,j);
+        return j;
+    }
+
+    static void swap(int[] arr,int a, int b){
+        int temp = arr[b];
+        arr[b] = arr[a];
+        arr[a] = temp;
+    }
+}
